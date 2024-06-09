0ef87f85a6041c39e421c3ffe80ba9f1ce9c4d49
梦念逍遥 <2589141604@qq.com>
2024-06-09 19:52:53 +0800

    update

--- module/gg_handler/gg_handler.py
+++ module/gg_handler/gg_handler.py
@@ -47,6 +47,9 @@ class GGHandler(ModuleBase):
                 exit(1)
             crashed = True
 
+    def gg_on(self):
+        return GGData(config=self.config).get_data(target='gg_on')
+
     def set(self, mode=True):
         """
             Set the GG status to True/False.
@@ -56,13 +59,13 @@ class GGHandler(ModuleBase):
         if mode:
             logger.hr('Enable GG', level=2)
             GGScreenshot(config=self.config, device=self.device).run(factor=self.factor)
+            logger.info(f'[Enabled]{self.gg_enable} [AutoRestart]{self.gg_restart} [CurrentStage]{self.gg_on()}')
         else:
             self.gg_reset()
 
     def check_config(self):
-        _gg_on = GGData(config=self.config).get_data(target='gg_on')
         logger.hr('Check GG config')
-        logger.info(f'[Enabled]{self.gg_enable} [AutoRestart]{self.gg_restart} [CurrentStage]{_gg_on}')
+        logger.info(f'[Enabled]{self.gg_enable} [AutoRestart]{self.gg_restart} [CurrentStage]{self.gg_on()}')
 
     def handle_restart_before_tasks(self) -> bool:
         """
@@ -81,10 +84,10 @@ class GGHandler(ModuleBase):
         Handle the restart errors of GG.
         """
         if self.gg_enable:
-            GGData(config=self.config).set_data(target='gg_on', value=False)
-            gg_on = GGData(config=self.config).get_data(target='gg_on')
+            if self.gg_on():
+                GGData(config=self.config).set_data(target='gg_on', value=False)
             logger.hr('Load GG config')
-            logger.info(f'[Enabled]{self.gg_enable} [AutoRestart]{self.gg_restart} [CurrentStage]{gg_on}')
+            logger.info(f'[Enabled]{self.gg_enable} [AutoRestart]{self.gg_restart} [CurrentStage]{self.gg_on()}')
 
     def check_status(self, mode=True):
         """
@@ -92,23 +95,21 @@ class GGHandler(ModuleBase):
         Args:
             mode: The multiplier status when finish the check.
         """
-        gg_on = GGData(config=self.config).get_data(target='gg_on')
         if self.gg_enable:
             logger.hr('Check GG status')
-            logger.info(f'[Enabled]{self.gg_enable} [AutoRestart]{self.gg_restart} [CurrentStage]{gg_on}')
+            logger.info(f'[Enabled]{self.gg_enable} [AutoRestart]{self.gg_restart} [CurrentStage]{self.gg_on()}')
             enable = mode if self.gg_restart else False
             if enable:
-                if not gg_on:
+                if not self.gg_on():
                     self.set(True)
-            elif gg_on:
+            elif self.gg_on():
                 self.gg_reset()
 
     def gg_reset(self):
         """
         Force restart the game to reset GG status to False
         """
-        gg_on = GGData(config=self.config).get_data(target='gg_on')
-        if self.gg_enable and gg_on:
+        if self.gg_enable and self.gg_on():
             logger.hr('Disable GG', level=2)
             self.restart()
             logger.attr('GG', 'Disabled')
